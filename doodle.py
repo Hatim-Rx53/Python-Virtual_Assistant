4e93e84f41aa72617da7342553f05f59e7f60410
Hatim-Rx53 <hatimrampurawala9@outlook.com>
2024-12-31 14:45:29 +0530

    update

--- doodle.py
+++ doodle.py
@@ -48,7 +48,7 @@ def takeCommand():
 		
 		# seconds of non-speaking audio before 
 		# a phrase is considered complete
-		r.pause_threshold = 0.35
+		r.pause_threshold = 0.25
 		audio = r.listen(source)
 		
 		# Now we will be using the try and catch
